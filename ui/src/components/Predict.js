284b995dc5b8e14a5937453a94345044bbc2e1c6
Nathan LeRoy <NLeRoy917@gmail.com>
2020-11-27 09:10:21 -0500

    migrate to cra for better dev

--- ui/src/components/Predict.js
+++ ui/src/components/Predict.js
@@ -8,10 +8,15 @@ const Predict = (props) => {
     return(
         <>
           <div className={styles.layout}>
+          <h1>Predict Popularity Scores</h1>
+           <div className={styles.buttonWrapper}>
             <button className={styles.generateButton}>
               Random
             </button>
-            
+            <button className={styles.generateButton}>
+              Test on Song
+            </button>
+           </div>
           </div>
         </>
     )
